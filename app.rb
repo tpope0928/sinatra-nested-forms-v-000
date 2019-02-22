683e1f11ec84eec940e280d4854424d37efe483b
Thomas Pope <tpope0928@gmail.com>
2019-02-22 19:57:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,5 +5,22 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+    end
   end
 end
