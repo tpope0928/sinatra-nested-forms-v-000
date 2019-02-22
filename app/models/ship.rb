632ed4ca98617f67686a7aa04d88f01c42119810
Thomas Pope <tpope0928@gmail.com>
2019-02-22 19:50:00 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,14 +1,14 @@
 class Ship
   attr_reader :name, :type, :booty
- 
+
   SHIPS = []
- 
+
   def initialize(args)
     @name = args[:name]
     @type = args[:type]
     SHIPS << self
   end
- 
+
   def self.all
     SHIPS
   end
