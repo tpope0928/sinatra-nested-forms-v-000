9f57cc3b035d8b81afd0cbb0be368703883837fc
Thomas Pope <tpope0928@gmail.com>
2019-02-22 19:42:03 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,12 @@
 class Pirate
+
+  attr_reader :name, :weight, :height
+
+  PIRATE = []
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+  end
 end
